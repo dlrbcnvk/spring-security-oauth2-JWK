a72caeac0501d7ea191da870104146c217db0cb6
dlrbcnvk <dlrbcnvk@naver.com>
2023-05-17 14:41:06 +0900

    spring security JWK 강의 관련 학습 프로젝트입니다.

---
+++ src/main/java/io/security/oauth2/springsecurityoauth2/SpringSecurityOauth2Application.java
@@ -0,0 +1,9 @@
+package io.security.oauth2.springsecurityoauth2;
+
+import static io.security.oauth2.springsecurityoauth2.JWKTest.jwk;
+
+public class SpringSecurityOauth2Application {
+    public static void main(String[] args) throws Exception {
+        jwk();
+    }
+}
